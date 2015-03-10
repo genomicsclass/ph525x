319410670991abdbe68dff8db20b1aefd9649296
Vince Carey <stvjc@channing.harvard.edu>
2015-03-10 11:12:16 -0400

    various to support course 4

---
+++ R/pngs.R
@@ -0,0 +1,19 @@
+
+#seqcAbst = function() {
+# f = readPNG(system.file("pngs/seqc.png", package="ph525x"))
+# grid.raster(f)
+#}
+
+prender = function(pref) {
+ newf = sub("%%PREF%%", pref, "pngs/%%PREF%%.png")
+ f = readPNG(system.file(newf, package="ph525x"))
+ dev.off()
+ grid.raster(f)
+}
+
+seqcAbst = function() prender("seqc")
+seqcDesign = function() prender("seqcDesign")
+obesTitle = function() prender("obesTitle")
+obesManh = function() prender("obesManh")
+esetDet1 = function() prender("esetDet1")
+esetDet2 = function() prender("esetDet2")
